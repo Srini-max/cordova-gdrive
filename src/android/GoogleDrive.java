655faebcd1c66f8f92f2c89066be6afc9da80096
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-07-28 04:30:01 +0400

    Update GoogleDrive.java

--- src/android/GoogleDrive.java
+++ src/android/GoogleDrive.java
@@ -565,16 +565,14 @@ Log.i(TAG, "MetaGETisFolder()"+ metadata.isFolder());
 
                 //DriveFile driveFile = .asDriveFile();
                 JSONObject object = new JSONObject();
+                String driveFilenameStr = metadata.getTitle().encodeToString();
                 String driveFileIdStr = metadata.getDriveId().encodeToString();
-                Log.i(TAG, driveFileIdStr+" !!!!!!!!!!!!!!!!!!!!!encoded string");
+                Log.i(TAG, driveFileIdStr+" !!!!!!!!!!!!!!!!!!!!!driveFileIdStr encoded string");
+                                Log.i(TAG, driveFilenameStr+" !!!!!!!!!!!!!!!!!!!!!driveFilenameStr encoded string");
                 String description = metadata.getDescription();
                 object.put(DRIVE_ID_DIC_KEY, driveFileIdStr);
                 object.put(DESCRIPTION_DRIVE_DIC_KEY, description);
                 elements.put(object);
-
-                Log.i(TAG, "MetaGETDESC"+ metadata.getDescription());
-                Log.i(TAG, "MetaGETFILEID"+driveFileIdStr);
-
             }
         }
         return elements;
