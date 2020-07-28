6e3f85045e63fd7b5777cdb1ba171fcefdfa4f24
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-07-28 04:41:56 +0400

    Update GoogleDrive.java

--- src/android/GoogleDrive.java
+++ src/android/GoogleDrive.java
@@ -550,7 +550,10 @@ public class GoogleDrive extends CordovaPlugin {
     }
 
     private JSONArray queryAllAppFiles() throws Exception {
-        Query query = new Query.Builder().addFilter(Filters.ownedByMe()).build();
+        SortOrder sortOrder = new SortOrder.Builder().addSortAscending(SortableField.TITLE).build();
+Query query = new Query.Builder().setSortOrder(sortOrder).build();
+
+       // Query query = new Query.Builder().addFilter(Filters.ownedByMe()).build();
                 Log.i(TAG, "Enetering  GDrive view list"+query);
         Task<MetadataBuffer> queryTask = mDriveResourceClient.query(query);
         MetadataBuffer metadataBuffer = Tasks.await(queryTask);
